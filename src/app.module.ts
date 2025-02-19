6504087bbf2c6dfbb1de5482a9d55913da150561
keodanic <victordanoelsc14@gmail.com>
2025-02-19 02:42:18 -0300

    feat:Adicionado as tabelas no Prisma e criado o módulo de qualidade de ar

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SensorController } from './sensor/sensor.controller';
+import { AirQualityModule } from './modules/air_quality/air_quality.module';
 
 @Module({
-  imports: [],
+  imports: [AirQualityModule],
   controllers: [AppController, SensorController],
   providers: [AppService],
 })
