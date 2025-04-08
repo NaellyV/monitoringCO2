8d9aa59f9e28c11927b1b1a1b269adfd82833723
NaellyV <naellyvitoria8@gmail.com>
2025-04-08 18:53:37 -0300

    fix: corrigindo bug nos dias da semana

--- src/modules/sensor/sensor.controller.ts
+++ src/modules/sensor/sensor.controller.ts
@@ -79,7 +79,7 @@ export class SensorController {
       }
     }
 
-    const diasSemana = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
+    const diasSemana = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
 
     const formattedData = Object.entries(agrupadoPorDia).map(([dia, { total, count }]) => {
       const data = new Date(dia);
